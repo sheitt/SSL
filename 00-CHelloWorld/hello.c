c7bbb7e5d189b7f3992907be648c622603581055
sheitt <sheit@frba.utn.edu.ar>
2026-04-12 12:11:42 -0300

    TP0 Finalizado(leyendo archivos)

--- 00-CHelloWorld/hello.c
+++ 00-CHelloWorld/hello.c
@@ -5,12 +5,5 @@ int main()
     //escribo el mensaje Hola Mundo! en stdout
     printf("Hola Mundo!\n");
 
-    //redirecciono la salida al archivo output.txt
-    FILE* f = freopen("output.txt", "w", stdout );
-    printf("Hola Mundo! 2 y 3\n");
-
-    //cierro el archivo output.txt
-    fclose(f);
-
     return 0;
 }
